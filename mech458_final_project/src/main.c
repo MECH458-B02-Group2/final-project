c9f7b43c0fb856aab3518b698b71ef6f407e02f6
malakivandas <malakivandas@gmail.com>
2021-03-27 16:11:01 -0700

    Cleaned up main.c & updated README.md

--- mech458_final_project/src/main.c
+++ mech458_final_project/src/main.c
@@ -1,25 +1,6 @@
-// READ EVERY SINGLE WORD IN THIS PIECE OF CODE...IF YOU DON'T YOU WILL NOT UNDERSTAND THIS!!!!!!!
-// READ EVERY SINGLE WORD IN THIS PIECE OF CODE...IF YOU DON'T YOU WILL NOT UNDERSTAND THIS!!!!!!!
-// READ EVERY SINGLE WORD IN THIS PIECE OF CODE...IF YOU DON'T YOU WILL NOT UNDERSTAND THIS!!!!!!!
-// READ EVERY SINGLE WORD IN THIS PIECE OF CODE...IF YOU DON'T YOU WILL NOT UNDERSTAND THIS!!!!!!!
-// READ EVERY SINGLE WORD IN THIS PIECE OF CODE...IF YOU DON'T YOU WILL NOT UNDERSTAND THIS!!!!!!!
-// READ EVERY SINGLE WORD IN THIS PIECE OF CODE...IF YOU DON'T YOU WILL NOT UNDERSTAND THIS!!!!!!!
-
-// Open up the document in START -> WinAVR -> AVR LibC -> User Manual -> avr/interrupt.h
-// Chapter 15, in Full Manual... THIS HAS A LOT OF IMPORTANT INFO...I have mentioned this at least 3 times!!!
-
-// For those that are still having major problems, I've seen about 1/3 of the class with major problems in
-// code structure. If you are still having major problems with your code, it's time to do a VERY quick overhaul.
-// I've provided a skeleton structure with an example using two input capture interrupts on PORTDA0 and A3
-// Please try this in the debugger.
-
-// Create a watch variable on STATE. To do this right click on the variable STATE and then
-// Add Watch 'STATE'. You can see how the variable changes as you click on PINDA0 or PINDA3. Note that the interrupt
-// catches a rising edge. You modify this to suit your needs.
-
 #include <avr/interrupt.h>
 #include <avr/io.h>
-#include <mainHeader.h>
+#include "mainHeader.h"
 
 // Global Variable---------
 // State Machine
