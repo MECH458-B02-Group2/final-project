9c5b7cc323eefa8cb6dc3c682b072b722d9f59f4
malakivandas <malakivandas@gmail.com>
2021-03-30 14:51:52 -0700

    Cleaned up code Organized main and header files and added Debug folder to gitignore

--- project/src/mainHeader.h
+++ project/src/mainHeader.h
@@ -1,74 +1,58 @@
-/* Type definitions */
+//------------------------------------------------------------------------------------------------------//
+// GLOBAL VARIABLES ------------------------------------------------------------------------------------//
+//------------------------------------------------------------------------------------------------------//
+
+// State Machine
+volatile char STATE;
+
+// Stepper Motor
+volatile int PolePosition;
+volatile int CurPosition;
+int const delay = 20;
+
+// DC Motor
+int const Speed = 0x50;
+
+// Other
+volatile unsigned char ADC_result;
+volatile unsigned int ADC_result_flag;
+volatile unsigned int Escape; // Probably WONT NEED, was for the end routine interrupt for lab 4b
+
+//------------------------------------------------------------------------------------------------------//
+// TYPE DECLARATIONS -----------------------------------------------------------------------------------//
+//------------------------------------------------------------------------------------------------------//
+
+// Linked Queue
 typedef struct {
   int ferro; // Need to distinguish between aluminum and steel
 	int reflect; // Need to distinguish between white and black delrin
 } element;
 
 typedef struct link{
-	element		e;
+	element e;
 	struct link *next;
 } link;
 
+//------------------------------------------------------------------------------------------------------//
+// COMPONENT SUBROUTINES -------------------------------------------------------------------------------//
+//------------------------------------------------------------------------------------------------------//
 
+// Stepper
+void initialize(void);
+void stepcw(int step);
+void stepccw(int step);
 
-/****************************************************************************************
-*  DESC: Accepts as input a new link by reference, and assigns the head and tail
-*  of the queue accordingly
-*  INPUT: the head and tail pointers, and a pointer to the new link that was created
-*/
-/* will put an item at the tail of the queue */
-void enqueue(link **h, link **t, link **nL){
+// DC
+void PWM(void);
+void DC_Start(void);
+void DC_Stop(void);
 
-	if (*t != NULL){
-		/* Not an empty queue */
-		(*t)->next = *nL;
-		*t = *nL; //(*t)->next;
-	}/*if*/
-	else{
-		/* It's an empty Queue */
-		//(*h)->next = *nL;
-		//should be this
-		*h = *nL;
-		*t = *nL;
-	}/* else */
-	return;
-}/*enqueue*/
+// Other
+void mTimer(int count);
 
+//------------------------------------------------------------------------------------------------------//
+// LINKED QUEUE SUBROUTINES ----------------------------------------------------------------------------//
+//------------------------------------------------------------------------------------------------------//
 
-
-
-/**************************************************************************************
-* DESC : Removes the link from the head of the list and assigns it to deQueuedLink
-* INPUT: The head and tail pointers, and a ptr 'deQueuedLink'
-* 		 which the removed link will be assigned to
-*/
-/* This will remove the link and element within the link from the head of the queue */
-void dequeue(link **h, link **t, link **deQueuedLink){
-	/* ENTER YOUR CODE HERE */
-	*deQueuedLink = *h;	// Will set to NULL if Head points to NULL
-	/* Ensure it is not an empty queue */
-	if (*h != NULL){
-		*h = (*h)->next;
-	}/*if*/
-  
-  /* If the queue is empty and h is NULL, set t to NULL */
-  if (*h == NULL){
-    *t = NULL;
-  }
-
-	return;
-}/*dequeue*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+void enqueue(link **h, link **t, link **nL);
+void dequeue(link **h, link **t, link **deQueuedLink);
